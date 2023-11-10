d41cc17ec9ecad26c3fde765bb36ff886d81a087
Caique Meira Oliveira <caique_m_oliveira@icloud.com>
2023-11-09 21:04:28 -0300

    feat(functionalApproach): use a monad for handling the API response

--- js_using_a_functional_approach/public/app/invoices/service.js
+++ js_using_a_functional_approach/public/app/invoices/service.js
@@ -1,15 +1,17 @@
 import { statusHandler, partialize, pipe } from "../utils/helpers.js";
+import { Maybe } from "../utils/maybe.js";
 
 const URL = "http://localhost:3000/notas";
 
-const getItemsFromNotes = (notes) => notes.flatMap(({ itens }) => itens);
-const filterInvoicesByCode = (code, notes) => notes.filter(({ codigo }) => codigo === code);
-const sumInvoicesValue = notes => notes.reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotes = (notesM) => notesM.map(notes => notes.flatMap(({ itens }) => itens));
+const filterInvoicesByCode = (code, notesM) => notesM.map(notes => notes.filter(({ codigo }) => codigo === code));
+const sumInvoicesValue = notesM => notesM.map(notes => notes.reduce((total, item) => total + item.valor, 0));
 
 const invoicesService = {
   listAll() {
     return fetch(URL)
       .then(statusHandler)
+      .then(Maybe.of)
       .catch(error => {
         console.error(error);
         return Promise.reject('Notas fiscais indisponíveis.')
@@ -20,7 +22,9 @@ const invoicesService = {
     const filterInvoices = partialize(filterInvoicesByCode, code)
     const sumInvoices = pipe(getItemsFromNotes, filterInvoices, sumInvoicesValue)
 
-    return this.listAll().then(sumInvoices)
+    return this.listAll()
+      .then(sumInvoices)
+      .then(totalM => totalM.getOrElse(0))
   }
 };
 
