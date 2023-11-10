24edd9b7eb51557475d9991214d234f0f55574a9
Caique Meira Oliveira <caique_m_oliveira@icloud.com>
2023-11-09 23:09:15 -0300

    chore(iosTraining): add notes about the first class

---
+++ iosTraining/class0.playground/Contents.swift
@@ -0,0 +1,27 @@
+/**
+ We're definyng a more declarative type.
+ */
+typealias Student = String
+
+var student0: Student = "Daniel"
+var student1: Student = "Luan"
+
+// --- Strings ---
+let name = "Caique"
+let sex: Character = "M"
+var age = 23
+var isStudent = true // :Bool
+
+print("Hi! My name is \(name)")
+
+// --- Math Operations ---
+5 / 2 // 2
+5.0 / 2 // 2.5
+Double(5) / 2 // 2.5
+
+var two = 2
+two += 1
+two -= 1
+two /= 1
+two *= 1
+two %= 2
