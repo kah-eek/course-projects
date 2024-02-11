b760220cd1a33dcc5e4ffc0770b17c29ca4ab343
Caique M. Oliveira <caique_m_oliveira@icloud.com>
2024-02-11 01:51:15 -0300

    feat: create the edit view

--- Scrumdinger/Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Scrumdinger/Models/DailyScrum.swift
@@ -11,8 +11,16 @@ struct DailyScrum: Identifiable {
     let id: UUID
     var title: String
     var attendees: [Attendee]
-    var lengthInMinutes: Int
     var theme: Theme
+    var lengthInMinutes: Int
+    var lengthInMinutesAsDouble: Double {
+        get {
+            Double(lengthInMinutes)
+        }
+        set {
+            lengthInMinutes = Int(newValue)
+        }
+    }
     
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
         self.id = id
@@ -32,6 +40,10 @@ extension DailyScrum {
             self.id = id
             self.name = name
         }
+    }
+    
+    static var emptyScrum: DailyScrum {
+        DailyScrum(title: "", attendees: [], lengthInMinutes: 5, theme: .sky)
     }
 }
 
