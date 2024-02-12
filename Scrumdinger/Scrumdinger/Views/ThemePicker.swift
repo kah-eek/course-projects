3ab6dd1bf666155d0715eabc575e377bff19cd47
Caique M. Oliveira <caique_m_oliveira@icloud.com>
2024-02-12 14:04:04 -0300

    refactor: move all view into the views group

---
+++ Scrumdinger/Scrumdinger/Views/ThemePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ThemePickerView.swift
+//  Scrumdinger
+//
+//  Created by Caique Meira Oliveira on 12/02/24.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemePicker()
+}
